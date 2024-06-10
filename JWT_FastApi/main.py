226d2a123f781e25fd34bfdab6b2e2d5a80a9e1b
Iván Rojas <irojas@tecnopress.pe>
2024-06-10 17:57:48 -0500

    avance parcial

--- JWT_FastApi/main.py
+++ JWT_FastApi/main.py
@@ -1,14 +1,36 @@
 import uvicorn
 import requests
-from fastapi import FastAPI, Body, Depends
+from typing import Annotated
+from fastapi import FastAPI, Body, Depends, Request, Form
+from fastapi.templating import Jinja2Templates
 from app.model import PostSchema, UserSchema, UserLoginSchema
 from app.auth.jwt_handler import signJWT
 from app.auth.jwt_bearer import jwtBearer
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 
-users = []
+Jinja2_template = Jinja2Templates(directory="templates")
+
+
+users = [
+    {    
+    "fullname" : "irojasc",
+    "email" : "irojasc",
+    "password" : "irojas"
+    }
+]
 posts = []
 
+
+#add new post
+@app.get("/", response_class=HTMLResponse, tags=["root"])
+def root(request: Request):
+    return Jinja2_template.TemplateResponse("index.html", {"request": request})
+
+#add new post
+@app.get("/users/dashboard", response_class=HTMLResponse)
+def dashboard(request: Request):
+    return Jinja2_template.TemplateResponse("dashboard.html", {"request": request})
 
 #add new post
 @app.get("/posts", tags=["posts"])
@@ -22,6 +44,26 @@ def get_post():
         "info": "Nothing to show you!"
         }  
 
+
+#add new post
+# @app.post("/users/login", dependencies = [Depends(jwtBearer())], tags=["posts"])
+@app.post("/users/login")
+# def login(user : UserLoginSchema = Body(default=None)):
+def login(email : Annotated[str, Form()], password : Annotated[str, Form()]):
+    return {
+        # "email": user.email,
+        # "password": user.password
+        "email": email,
+        "password": password
+    }
+    # if check_user(user):
+    #     return signJWT(user.email)
+    # else:
+    #     return {
+    #         "error": "Invalid login details!"
+    #     }
+
+        
 #add new post
 @app.post("/posts", dependencies = [Depends(jwtBearer())], tags=["posts"])
 def add_post(post : PostSchema):
@@ -46,20 +88,13 @@ def check_user(data: UserLoginSchema):
         return False
 
 
-#login a registered user
-@app.post("/user/login", tags=["user"])
-def user_login(user : UserLoginSchema = Body(default=None)):
-    if check_user(user):
-        return signJWT(user.email)
-    else:
-        return {
-            "error": "Invalid login details!"
-        }
 
 #temporary post
 @app.get("/product/{pattern}", tags=["posts"])
 def add_post(pattern):
+    # results = requests.get(f'https://nodejs-mysql-restapi-production-5162.up.railway.app/api/products/{pattern}')
     results = requests.get(f'https://nodejs-mysql-restapi-production-5162.up.railway.app/api/products/{pattern}')
+    # https://api.callmebot.com/whatsapp.php?phone=51935017677&text=This+is+a+test&apikey=9817632
     return {
         "value returned": results.json()
     }
